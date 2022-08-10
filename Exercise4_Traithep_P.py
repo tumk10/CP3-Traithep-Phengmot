e0abd3c1b630201aadd9d45ad736a8c15f715583
tumk10 <110971215+tumk10@users.noreply.github.com>
2022-08-10 18:27:17 +0700

    Create Exercise4_Traithep_P.py
    test

---
+++ Exercise4_Traithep_P.py
@@ -0,0 +1,10 @@
+foueng = 75.5
+genbus = 88.0
+intcomsys = 99.0
+compro = 89.5
+
+print("--- Your Score ---")
+print("Foundation English :",foueng)
+print("General Business :",genbus)
+print("Introduction to Computer Systems :",intcomsys)
+print("Computer Programming :",compro)
